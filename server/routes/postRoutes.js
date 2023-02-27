cb98fcec8fa5d6e66603f1d06f38877b80e83c4d
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-02-27 10:52:57 +0300

    implemented create / find logic in postRoutes with cloudinary

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,4 +8,39 @@ dotenv.config()
 
 const router = express.Router()
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINRAY_CLOUD_NAME,
+  api_key: process.env.CLOUDINRAY_API_KEY,
+  api_secret: process.env.CLOUDINRAY_API_SECRET,
+})
+
+// GET ALL POSTS
+router.route('/').get(async (req, res) => {
+  try {
+    const posts = await Post.find({})
+
+    res.status(200).json({ sucess: true, data: posts })
+  } catch (error) {
+    res.status(500).json({ sucess: false, message: error })
+  }
+})
+
+// CREATE A POST
+router.route('/').post(async (req, res) => {
+  try {
+    const { name, prompt, photo } = req.body
+    const photoUrl = await cloudinary.uploader.upload(photo)
+
+    const newPost = await Post.create({
+      name,
+      prompt,
+      photo: photoUrl.url,
+    })
+
+    res.status(201).json({ sucess: true, data: newPost })
+  } catch (error) {
+    res.status(500).json({ sucess: false, message: error })
+  }
+})
+
 export default router
